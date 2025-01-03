568443aa2da4bcaba004acfdf6812ff06a6bf569
Klaudio Merja <merjakla03@gmail.com>
2025-01-03 15:40:03 +0100

    Pulizia del vecchio POC

--- data/nifi/python_extensions/model/models.py
+++
@@ -1,6 +0,0 @@
-def load_model(name, kwargs):
-    if name == 'ChatOpenAI':
-        from langchain_openai import ChatOpenAI
-        return ChatOpenAI(**kwargs)
-    else:
-        raise Exception("Unknown model: " + name)
--- data/nifi/python_extensions/retriever/embeddings.py
+++
@@ -1,6 +0,0 @@
-def load_embedding(name, kwargs):
-    if name == 'OpenAIEmbeddings':
-      from langchain_openai import OpenAIEmbeddings
-      return OpenAIEmbeddings(**kwargs)
-    else:
-        raise Exception("Unknown embedding: " + embedding)
