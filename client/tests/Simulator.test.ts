cf061c7fff48aeb413afea485c992ec4cb406efb
aprecoma <andreaprecoma20@gmail.com>
2025-03-09 00:17:08 +0100

    configurato l'ambiente di variabili, test e report coverage

---
+++ client/tests/Simulator.test.ts
@@ -0,0 +1,9 @@
+describe('Simulator', () => {
+    it('should remove rent ended', () => {
+        
+    });
+
+    it('should receive rent ended update', () => {
+
+    });
+});
--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    // TODO:
-  },
+    test: {
+        globals: true,
+        // environment: 'node', ???
+
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+
+            exclude: [
+                '.nvmrc',
+                '.prettierrc',
+                'Dockerfile',
+                'env-var.env',
+                'package-lock.json',
+                'package.json',
+                'tsconfig.json',
+                'vitest.config.ts',
+                'node_modules/**',
+                'test/**',
+                'coverage/**',
+            ],
+
+            thresholds: {
+                lines: 80,
+                functions: 80,
+                branches: 80,
+                statements: 80,
+            },
+        },
+    },
 });
