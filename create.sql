f4a2c292b3954955c575aaafc34582729cb0f009
Perruzz <perozzoandrea3@gmail.com>
2025-03-11 18:11:45 +0100

    uuid --> serial

--- create.sql
+++ create.sql
@@ -18,31 +18,31 @@ DROP TABLE IF EXISTS positions cascade;
 DROP TABLE IF EXISTS user_categories CASCADE;
 
 CREATE TABLE IF NOT EXISTS users (
-    id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     preferences VARCHAR(200)
 );
 
 CREATE TABLE IF NOT EXISTS user_categories (
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     category category_enum NOT NULL,
     PRIMARY KEY (user_id, category)
 );
 
 CREATE TABLE IF NOT EXISTS bikes(
-    id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY
+    id SERIAL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS rents(
-    id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    bike_id UUID NOT NULL REFERENCES bikes(id) ON DELETE CASCADE,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    id SERIAL PRIMARY KEY,
+    bike_id INT NOT NULL REFERENCES bikes(id) ON DELETE CASCADE,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     is_closed BOOL NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS positions(
-    id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    rent_id UUID NOT NULL REFERENCES rents(id) ON DELETE CASCADE,
+    id SERIAL PRIMARY KEY,
+    rent_id INT NOT NULL REFERENCES rents(id) ON DELETE CASCADE,
     latitude REAL NOT NULL,
     longitude REAL NOT NULL
 );
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS merchants(
 );
 
 CREATE TABLE IF NOT EXISTS points_of_interest (
-    id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     merchant_vat CHARACTER VARYING(11) NOT NULL REFERENCES merchants(vat) ON DELETE CASCADE,
     name TEXT NOT NULL,
     start_at TIME NOT NULL,
@@ -64,8 +64,8 @@ CREATE TABLE IF NOT EXISTS points_of_interest (
 );
 
 CREATE TABLE IF NOT EXISTS advertisements (
-    rent_id UUID NOT NULL REFERENCES rents(id) ON DELETE CASCADE,
-    poi_id  UUID NOT NULL REFERENCES points_of_interest(id) ON DELETE CASCADE,
+    rent_id INT NOT NULL REFERENCES rents(id) ON DELETE CASCADE,
+    poi_id  INT NOT NULL REFERENCES points_of_interest(id) ON DELETE CASCADE,
     adv TEXT,
     PRIMARY KEY (rent_id, poi_id)
 );
