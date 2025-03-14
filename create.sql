a6c474ebc90d5197523f2462ad2653056c65da6f
Andrea Perozzo <148521685+Perruzz@users.noreply.github.com>
2025-03-14 11:12:04 +0100

    Modifiche all'SQL

--- create.sql
+++ create.sql
@@ -36,18 +36,18 @@ CREATE TABLE bikes
 
 CREATE TABLE rents
 (
-    id        SERIAL PRIMARY KEY,
+    id        UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
     bike_id   INT  NOT NULL REFERENCES bikes (id) ON DELETE CASCADE,
     user_id   INT  NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    is_closed BOOL NOT NULL DEFAULT FALSE
+    is_closed BOOL NOT NULL    DEFAULT FALSE
 );
 
 CREATE TABLE positions
 (
-    time_stamp TIMESTAMP WITH TIME ZONE NOT NULL,
-    rent_id    INT                      NOT NULL REFERENCES rents (id) ON DELETE CASCADE,
-    latitude   REAL                     NOT NULL,
-    longitude  REAL                     NOT NULL,
+    time_stamp TIMESTAMP WITH TIME ZONE,
+    rent_id    UUID NOT NULL REFERENCES rents (id) ON DELETE CASCADE,
+    latitude   REAL NOT NULL,
+    longitude  REAL NOT NULL,
     PRIMARY KEY (time_stamp, rent_id)
 );
 
@@ -72,18 +72,34 @@ CREATE INDEX idx_points_of_interest_location ON points_of_interest USING GIST (S
 
 CREATE TABLE poi_hours
 (
-    poi_id      INT                 NOT NULL REFERENCES points_of_interest (id) ON DELETE CASCADE,
-    day_of_week INT                 NOT NULL CHECK (day_of_week <= 7 AND day_of_week >= 1),
+    poi_id      INT  NOT NULL REFERENCES points_of_interest (id) ON DELETE CASCADE,
+    day_of_week INT  NOT NULL CHECK (day_of_week <= 7 AND day_of_week >= 1),
     open_at     TIME WITH TIME ZONE NOT NULL,
     close_at    TIME WITH TIME ZONE NOT NULL,
-    CHECK (close_at < open_at),
+    CHECK (close_at > open_at),
     PRIMARY KEY (poi_id, day_of_week)
 );
 
 CREATE TABLE advertisements
 (
-    rent_id INT NOT NULL REFERENCES rents (id) ON DELETE CASCADE,
-    poi_id  INT NOT NULL REFERENCES points_of_interest (id) ON DELETE CASCADE,
+    rent_id UUID NOT NULL REFERENCES rents (id) ON DELETE CASCADE,
+    poi_id  INT  NOT NULL REFERENCES points_of_interest (id) ON DELETE CASCADE,
     adv     TEXT,
     PRIMARY KEY (rent_id, poi_id)
 );
+
+-- Non so i tuoi gusti Andre, solo a scopo di test
+INSERT INTO users(name, preferences)
+VALUES ('Andrea Perozzo', 'Odio gli hamburger e i fritti. Mi piace il ramen.');
+INSERT INTO user_interests
+values (1, 'fast_food');
+INSERT INTO bikes(id)
+VALUES (default);
+INSERT INTO rents(bike_id, user_id)
+VALUES (1, 1);
+INSERT INTO merchants
+VALUES ('IT101010101', 'Bozo');
+INSERT INTO points_of_interest(merchant_vat, name, latitude, longitude, category, description)
+VALUES ('IT101010101', 'Rameno', 78.5, 78.5, 'fast_food', 'ristorante di ramen.');
+INSERT INTO points_of_interest(merchant_vat, name, latitude, longitude, category, description)
+VALUES ('IT101010101', 'Frittos', 76.5, 76.5, 'fast_food', 'ristorante di hamburger e fritti.');
