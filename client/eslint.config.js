efdb2574fdf31deccdc71263a6d3c2cb8fc0316c
MarinDavide <marin.davide03@gmail.com>
2025-03-16 13:50:00 +0100

    fix eslint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -25,12 +25,8 @@ export default [
             "no-unused-vars": "off",                      // Disabilita la regola di default (consigliato per non dare falsi errori con la regola successiva)
             "@typescript-eslint/no-unused-vars": "error", // Segnala se ci sono variabili non utilizzate
             "@typescript-eslint/no-unnecessary-type-constraint": "error", // Segnala se la dichiarazione di un estende "any" o "unknown"
-            "@typescript-eslint/no-unnecessary-type-assertion": "error", // Segnala se se viene usato "as" per dire che un espressione è di tipo diverso da quello che ci si aspetta
             "@typescript-eslint/no-unsafe-declaration-merging": "error", // Segnala se si tenta di unire due dichiarazioni con lo stesso nome
             "@typescript-eslint/no-unsafe-function-type": "error", // Segnala se non viene specificato il tipo di ritorno di una funzione
-            "@typescript-eslint/no-unsafe-return": "error", // Segnala se una funzione ritorna un tipo non sicuro, come "any"
-            "@typescript-eslint/prefer-readonly": "error", // Segnala se una variabile privata, che non viene modificata al di fuori del costruttore, non è dichiarata come readonly
-            "@typescript-eslint/restrict-plus-operands": "error", // Segnala se si tenta di sommare due variabili di tipo diverso
             "@typescript-eslint/triple-slash-reference": "error", // Segnala se si utilizza "/// <reference path='...' />" invece di "import"
             "@typescript-eslint/no-extra-non-null-assertion": "error", // Segnala se si usa "!!" per forzare una variabile a non essere null (è inutile più di un !)
             "@typescript-eslint/no-inferrable-types": "error", // Segnala se si cerca di esplicitare il tipo di una variabile quando è già implicito (con int, string e boolean)
